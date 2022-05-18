8b5a67c731c989f673ef14fb81b3cc1188428a44
ejajahmad <ajasaifi2018@gmail.com>
2022-05-18 12:34:52 +0530

    Spinner: Installed, and Added a Ring Spinner

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { Rings } from "react-loader-spinner";
 
 export const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="flex justify-center items-center">
+      <Rings color="#00BFFF" />
+    </div>
+  );
 };
