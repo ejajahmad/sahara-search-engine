b977abef35c3a19baa12e5fa09e88d5c65c93ecd
ejajahmad <ajasaifi2018@gmail.com>
2022-05-18 10:42:55 +0530

    Navbar: Logo, Darkmode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+// Style Imports
+
+// React Imports
+import React, { useState } from "react";
+
+// External Libraries
+
+// Internal Imports
+import { Footer } from "./components/Footer";
+import { Navbar } from "./components/Navbar";
+import { Routes } from "./components/Routes";
+
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div className="App">
-      <h1 className="text-3xl ">Hello React!</h1>
+    <div className={darkTheme ? "dark" : ""}>
+      <div className=" bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+        <Routes darkTheme={darkTheme} />
+        <Footer />
+        <h1>React</h1>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import { MdDarkMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
+import { Search } from "./Search";
+
+export const Navbar = ({ darkTheme, setDarkTheme }) => {
+  return (
+    <div className=" p-5  flex flex-wrap justify-center align-middle sm:justify-between">
+      <div className="flex justify-between items-center w-screen">
+        <Link to="/">
+          <p className="text-2xl font-bold text-gray-800 dark:text-gray-200">Sahara</p>
+        </Link>
+
+        <Search />
+
+        <button
+          className=" bg-gray-900  text-gray-200 dark:bg-gray-100  dark:text-gray-800 p-2 rounded drop-shadow-md"
+          onClick={() => setDarkTheme(!darkTheme)}
+        >
+          {darkTheme ? (
+            <span className="flex flex-row items-center gap-2">
+              <MdLightMode /> Light Mode
+            </span>
+          ) : (
+            <span className="flex flex-row items-center gap-2">
+              <MdDarkMode /> Dark Mode
+            </span>
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
